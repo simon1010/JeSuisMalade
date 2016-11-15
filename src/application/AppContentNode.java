82ed9458c218f7a73a3f993b39da09fdc02ef1ca
Szabolcs <simon.szabolcs10@gmail.com>
2016-11-15 23:58:47 +0200

    Added ControlsFX to project Played around with the StatusBar and added first button

---
+++ src/application/AppContentNode.java
@@ -0,0 +1,37 @@
+package application;
+
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+
+import javafx.scene.Node;
+
+public class AppContentNode extends Node {
+
+	@Override
+	protected NGNode impl_createPeer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseBounds impl_computeGeomBounds(BaseBounds bounds, BaseTransform tx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean impl_computeContains(double localX, double localY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object impl_processMXNode(MXNodeAlgorithm alg, MXNodeAlgorithmContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,15 @@
 package application;
 	
+import org.controlsfx.control.StatusBar;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -16,6 +22,24 @@ public class Main extends Application {
 			// end DBC Part
 			
 			BorderPane root = new BorderPane();
+			
+			ToolBar toolbar = new ToolBar();
+		    StatusBar statusbar = new StatusBar();
+		    
+		    Button button = new Button("Click");
+            Label label = new Label("Not clicked");
+	        button.setOnAction(value ->  {
+	           statusbar.setText("OK -- Clicked Button");
+	           label.setText("Clicked!");
+	        });
+
+	        HBox hbox = new HBox(button, label);
+		    
+		    //Node appContent = new AppContentNode();
+		    root.setTop(toolbar);
+		    root.setCenter(hbox);
+		    root.setBottom(statusbar);
+		     
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
